73e8f2323050520017c7ce78fc4c2eb0a93ea206
Mohamed Shadab <shadabdragon@gmail.com>
2018-11-14 02:57:07 +0530

    Minor Changes and Bug Fixes

--- src/data/index.js
+++ src/data/index.js
@@ -1,8 +1,8 @@
 export const batches = [
-  { value: 1701, label: "BTech 2017" },
-  { value: 1601, label: "BTech 2016" },
-  { value: 1501, label: "BTech 2015" },
-  { value: 1711, label: "MTech 2017" },
-  { value: 1712, label: "MscIT/Mdes 2017" },
-  { value: 1711, label: "PHD 2017" }
+  { value: "1701", label: "BTech 2017" },
+  { value: "1601", label: "BTech 2016" },
+  { value: "1711", label: "MTech 2017" },
+  { value: "1712", label: "MscIT/Mdes 2017" },
+  { value: "1811", label: "MTech 2018" },
+  { value: "1812", label: "MscIT/Mdes 2018" }
 ];
--- src/index.js
+++ src/index.js
@@ -37,4 +37,4 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-serviceWorker.register();
+serviceWorker.unregister();

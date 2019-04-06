3bee0615d78e8c931b971340b9edbc83da7d3e47
Mohamed Shadab <shadabdragon@gmail.com>
2019-04-06 08:39:32 +0530

    Fixs major bug Changes: - Adds a check for the presence of data in the CommitteeName component

--- src/components/UI/CommitteeName/index.js
+++ src/components/UI/CommitteeName/index.js
@@ -3,23 +3,25 @@ import { floorMapper } from "../../../utils/utilityFunctions";
 import { committeeMapDetailed, committeeMap } from "../../../data";
 
 function CommitteeName(props) {
-  if (props.useDetailed) {
-    if (committeeMapDetailed[props.name]) {
-      return <div>{committeeMapDetailed[props.name]}</div>;
-    } else if (props.name[1] === "/") {
-      return <div>{floorMapper(props.name)}</div>;
+  if (props.name) {
+    if (props.useDetailed) {
+      if (committeeMapDetailed[props.name]) {
+        return <div>{committeeMapDetailed[props.name]}</div>;
+      } else if (props.name[1] === "/") {
+        return <div>{floorMapper(props.name)}</div>;
+      } else {
+        return <div>{props.name}</div>;
+      }
     } else {
-      return <div>{props.name}</div>;
+      if (committeeMap[props.name]) {
+        return committeeMap[props.name];
+      } else if (props.name[1] === "/") {
+        return <div>{floorMapper(props.name)}</div>;
+      } else {
+        return <div>{props.name}</div>;
+      }
     }
-  } else {
-    if (committeeMap[props.name]) {
-      return committeeMap[props.name];
-    } else if (props.name[1] === "/") {
-      return <div>{floorMapper(props.name)}</div>;
-    } else {
-      return <div>{props.name}</div>;
-    }
-  }
+  } else return <div />;
 }
 
 export default CommitteeName;
